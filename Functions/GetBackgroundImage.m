42ac30ae112b7b065698705f8b60e6870a09725e
SponTanious <dani.tanious@hotmail.com>
2016-11-03 00:29:10 +1000

    Domino Detection perfection

--- Functions/GetBackgroundImage.m
+++ Functions/GetBackgroundImage.m
@@ -1,10 +1,10 @@
-function [ backgroundImage ] = GetBackgroundImage(colourDevice)
+function [ backgroundImage ] = GetBackgroundImage(image)
 %UNTITLED5 This function saves the original background image for
 %   comparison with a cluttered image.
 
 step(colourDevice);
 
-backgroundImage = step(colourDevice);
+backgroundImage = image;
 
 end
 
