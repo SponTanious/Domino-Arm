63fcf7499583d9a3227ca04aae8469a802703dfa
samuelcomerford <samcomerford@outlook.com>
2016-11-02 12:17:51 +1000

    Correct Motor Movement

--- Functions/moveMotors.m
+++ Functions/moveMotors.m
@@ -4,14 +4,16 @@ axratio = 3.41;
 mxratio = 11.377;
 a = size(id);
 
-for x = 1:a(2)
+for x = 1:2
     if (id(x) == 1)
-        dyna_degrees = (goal_pos(x))*mxratio
+        dyna_degrees = (goal_pos(x))*mxratio;
         calllib('dynamixel','dxl_write_word', id(x), 30, dyna_degrees);
    
     else
-        dyna_degrees = (goal_pos(x)-30)*axratio
-        calllib('dynamixel','dxl_write_word', id(x), 30, dyna_degrees);
+        dyna_degrees2 = (goal_pos(x)-30)*axratio;
+        dyna_degrees3 = (goal_pos(x+1)-30)*axratio;
+        dyna_degrees4 = (goal_pos(x+2)-30)*axratio;
+        calllib('dynamixel','dxl_write_word', id(x), 30, dyna_degrees2, id(x+1), 30, dyna_degrees3, id(x+2), 30, dyna_degrees4);
     end
 end
 
