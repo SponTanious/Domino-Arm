f5b2f2b011fa81f985e37b12fffd59caf5dc24d7
samuelcomerford <samcomerford@outlook.com>
2016-11-02 16:06:56 +1000

    Motion edits

--- Functions/moveMotors.m
+++ Functions/moveMotors.m
@@ -4,16 +4,14 @@ axratio = 3.41;
 mxratio = 11.377;
 a = size(id);
 
-for x = 1:2
+for x = 1:a(2)
     if (id(x) == 1)
         dyna_degrees = (goal_pos(x))*mxratio;
         calllib('dynamixel','dxl_write_word', id(x), 30, dyna_degrees);
    
     else
-        dyna_degrees2 = (goal_pos(x)-30)*axratio;
-        dyna_degrees3 = (goal_pos(x+1)-30)*axratio;
-        dyna_degrees4 = (goal_pos(x+2)-30)*axratio;
-        calllib('dynamixel','dxl_write_word', id(x), 30, dyna_degrees2, id(x+1), 30, dyna_degrees3, id(x+2), 30, dyna_degrees4);
+        dyna_degrees = (goal_pos(x)-30)*axratio;
+        calllib('dynamixel','dxl_write_word', id(x), 30, dyna_degrees);
     end
 end
 
